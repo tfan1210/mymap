0a99709c75fae3e7f67d210ce309c37149b4bd5c
terence.fan <terence.fan@sha-50424-mbp.local>
2018-07-09 19:55:48 +0800

    From local to github

---
+++ README.md
@@ -0,0 +1,20 @@
+# Udacity Neighborhood Project
+Project of Udacity and JS framework & Google Map API.
+
+## Installation:
+ - Git clone the repo
+ - Run the index.html file
+
+
+## Functions of the program:
+- The markers are all of my favorite restaurant in SF.
+- Point of interests are loaded using Foursquare API
+- API messages are displayed at the top
+- Filters are on the sidebar
+- Responsive page
+- Knockout JS is used
+- Google map loaded using Maps API
+
+## References:
+- Foursquare API from https://developer.foursquare.com/
+- Google Maps API from https://developers.google.com/maps/
---
+++ js/starting-data.js
@@ -0,0 +1,68 @@
+// My list of baseline restaurant objects.  "ID" is the key for the Foursquare
+// API call.  Name is manually entered here for convenience.
+// Neighborhood data is manually entered; had a hard time finding a reasonable
+// API to automatically generate neighboor data by location.
+
+var restObjArray = [{
+    'name': 'Sweet Maple',
+    'id': '4cc0b6dced033704295da923',
+    'notes': 'This place is amazing for brunch! There was a line but it went by fast. Bottomless mimosas were great and the server always had our glasses full before we could even ask for more.',
+    'neighborhood': 'Lower Pacific Heights'
+  },
+
+  {
+    'name': 'Outerlands',
+    'id': '4a354111f964a520ad9c1fe3',
+    'notes': 'Small plates, nice and cozy environment, and great dishes! I like the Outerlands a lot! Service is nice, food is great, wine is decent and the environment is cozy and a bit hipster! This place is a win!',
+    'neighborhood': 'Outer Sunset'
+  },
+
+  {
+    'name': 'Yummy Yummy',
+    'id': '443fde11f964a5203a321fe3',
+    'notes': 'Great place to grab authentic pho, and seafood Vietnamese cusines. It also serves great Vietnamese sandwiches',
+    'neighborhood': 'Inner Sunset'
+  },
+
+  {
+    'name': 'Kitchen Story',
+    'id': '50ab8755e4b0869279a80872',
+    'notes': 'Kitchen Story is my favorite brunch spot in the city. The friendly staff and inventive food makes it a must.',
+    'neighborhood': 'Mission District'
+  },
+
+  {
+    'name': 'Perilla',
+    'id': '4b1b3d2bf964a52002fa23e3',
+    'notes': 'This is a solid place in the area for their delicious garlic noodles! Their five spice chicken and charboiled shrimp and beef options are all really good!',
+    'neighborhood': 'Inner Sunset'
+  },
+
+  {
+    'name': 'Sushirrito',
+    'id': '4feb58a1e4b084ec89e24d74',
+    'notes': 'Sushi, but presented in a burrito form? That is definitely an interesting concept.',
+    'neighborhood': 'Financial District'
+  },
+
+  {
+    'name': 'Smokestack',
+    'id': '541e02fd498e5af2958c8b1b',
+    'notes': 'We first tried the "meat stick" at a food fair in Dogpatch, and immediately resolved to actually visit the location.  The brisket melts in your mouth.  It is glorious.  All else is secondary.',
+    'neighborhood': 'Dogpatch'
+  },
+
+  {
+    'name': 'Lous Cafe',
+    'id': '4bc767fd8b7c9c747d6536cf',
+    'notes': 'This is my favorite sandwich joint in the city right now. This place can be compared to Mr. Pickles',
+    'neighborhood': 'Inner Richmond'
+  },
+
+  {
+    'name': 'MarketBar',
+    'id': '49ef6148f964a520ab681fe3',
+    'notes': 'A reasonably solid choice in terms of food options and quality, but the price reflects the surroundings.  Still, eating on the terrace under the sun, watching the tourists wander by - it\'s an enjoyable experience.',
+    'neighborhood': 'Embarcadero'
+  }
+];
